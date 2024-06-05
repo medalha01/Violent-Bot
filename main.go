4cc95d5019389e1438f95bc59326bbff29147d79
Isac Martins <hiperboss2009@gmail.com>
2024-06-04 23:43:33 -0300

    Suicidal bot is now online

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+// This function will be called (due to AddHandler below) every time a new message is created on any channel that the authenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore all messages created by the bot itself
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	// If the message contains "kill yourself", respond with "aaaa" and then exit
+	if strings.Contains(strings.ToLower(m.Content), "kill yourself") {
+		s.ChannelMessageSend(m.ChannelID, "aaaa")
+		fmt.Println("Kill yourself message detected. Bot is shutting down.")
+		s.Close()
+		os.Exit(0)
+	}
+}
+
+func main() {
+	// Replace with your bot token
+	Token := "tOKE"
+
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(messageCreate)
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running. Press CTRL+C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
